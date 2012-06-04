c288ec8ef14ecfd8317d8e354cc03dad9289ab6c
Tobias Bischoff <tobias.bischoff@gmail.com>
2012-06-04 21:08:01 +0200

    Initial Commit

---
+++ ServerMarket/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ServerMarket
+//
+//  Created by Bischoff Tobias on 04.06.12.
+//  Copyright (c) 2012 Tobias Bischoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ServerMarket/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ServerMarket
+//
+//  Created by Bischoff Tobias on 04.06.12.
+//  Copyright (c) 2012 Tobias Bischoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
